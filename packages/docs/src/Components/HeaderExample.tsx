74c58030dd7f9c092c30c112a56332955c376a95
Andrew Lisowski <lisowski54@gmail.com>
2024-08-03 16:01:00 -0700

    improve style a little

--- packages/docs/src/Components/HeaderExample.tsx
+++ packages/docs/src/Components/HeaderExample.tsx
@@ -6,34 +6,45 @@ import {
 } from "react-resizable-grid-panels";
 
 function PanelGroup(props: PanelGroupProps) {
-  return (
-    <PanelGroupPrimitive
-      {...props}
-      className={`border border-gray-400 ${props.className}`}
-    />
-  );
+  return <PanelGroupPrimitive {...props} className={`${props.className}`} />;
 }
 
 function Panel(props: any) {
-  return <PanelPrimitive {...props} className="bg-gray-200" />;
+  return (
+    <PanelPrimitive
+      {...props}
+      className="bg-gray-200 border rounded-xl overflow-hidden border-gray-400 flex items-center justify-center text-gray-800"
+    />
+  );
 }
 
 function PanelResizer(props: any) {
   return (
     <PanelResizerPrimitive
       {...props}
-      size="4px"
-      className="h-full bg-red-500"
-    />
+      size="20px"
+      className="h-full relative group"
+    >
+      <div
+        className="
+          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2 h-8 rounded-full
+          bg-gray-300 group-hover:bg-gray-400 group-data-[state='dragging']:bg-blue-500
+        "
+      />
+    </PanelResizerPrimitive>
   );
 }
 
 export default function HeaderExample() {
   return (
-    <PanelGroup id="group" className="max-w-xl w-full">
-      <Panel id="1">1</Panel>
+    <PanelGroup id="group" className="max-w-xl w-full" style={{ height: 200 }}>
+      <Panel min="60px" id="1">
+        1
+      </Panel>
       <PanelResizer id="2" />
-      <Panel id="3">3</Panel>
+      <Panel min="60px" id="3">
+        3
+      </Panel>
     </PanelGroup>
   );
 }
