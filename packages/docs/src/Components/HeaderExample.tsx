c2daa84affab25c8f8d385d66219825f6acd2d02
Andrew Lisowski <lisowski54@gmail.com>
2024-08-03 16:30:38 -0700

    make it look cool

--- packages/docs/src/Components/HeaderExample.tsx
+++ packages/docs/src/Components/HeaderExample.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 import {
   PanelGroup as PanelGroupPrimitive,
   PanelGroupProps,
   Panel as PanelPrimitive,
+  PanelProps,
   PanelResizer as PanelResizerPrimitive,
 } from "react-resizable-grid-panels";
 
@@ -9,12 +13,68 @@ function PanelGroup(props: PanelGroupProps) {
   return <PanelGroupPrimitive {...props} className={`${props.className}`} />;
 }
 
-function Panel(props: any) {
+const borders = {
+  green: "border-green-500",
+  red: "border-red-500",
+};
+
+const backgrounds = {
+  green: "bg-green-200 bg-opacity-60 text-green-800",
+  red: "bg-red-200 bg-opacity-60 text-red-800",
+};
+
+const solidBackgrounds = {
+  green: "bg-green-500 text-white",
+  red: "bg-red-500 text-white",
+};
+
+function Panel({ color, ...props }: PanelProps & { color: "green" | "red" }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState<
+    { width: number; height: number } | undefined
+  >();
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const observer = new ResizeObserver((entries) => {
+      const entry = entries[0];
+
+      if (!entry) {
+        return;
+      }
+
+      setDimensions({
+        width: entry.borderBoxSize[0].inlineSize,
+        height: entry.borderBoxSize[0].blockSize,
+      });
+    });
+
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [props.id]);
+
   return (
     <PanelPrimitive
+      ref={ref}
       {...props}
-      className="bg-gray-200 border rounded-xl overflow-hidden border-gray-400 flex items-center justify-center text-gray-800"
-    />
+      className={` bg-gray-200 border-4 overflow-hidden ${borders[color]} ${backgrounds[color]} flex items-center justify-center text-gray-800`}
+    >
+      {dimensions && (
+        <span className="flex items-center font-mono w-full">
+          <span className={`${solidBackgrounds[color]} h-1 mx-2 flex-1`} />
+          <span className={`${solidBackgrounds[color]} p-2`}>
+            {dimensions.width.toFixed(2)}px
+          </span>
+          <span className={`${solidBackgrounds[color]} h-1 mx-2 flex-1`} />
+        </span>
+      )}
+    </PanelPrimitive>
   );
 }
 
@@ -38,13 +98,9 @@ function PanelResizer(props: any) {
 export default function HeaderExample() {
   return (
     <PanelGroup id="group" className="max-w-xl w-full" style={{ height: 200 }}>
-      <Panel min="60px" id="1">
-        1
-      </Panel>
+      <Panel color="green" min="130px" id="1" />
       <PanelResizer id="2" />
-      <Panel min="60px" id="3">
-        3
-      </Panel>
+      <Panel color="red" min="130px" id="3" />
     </PanelGroup>
   );
 }
