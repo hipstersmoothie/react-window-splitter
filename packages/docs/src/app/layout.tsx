0d8a1a03f6682bd9247af1f8bda4215b8592caf6
Andrew Lisowski <lisowski54@gmail.com>
2024-08-03 16:10:56 -0700

    convert to app router

---
+++ packages/docs/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "react-resizable-grid-panels",
+  description: "A full featured resizable grid layout for React",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
--- packages/docs/src/app/page.tsx
+++ packages/docs/src/app/page.tsx
@@ -1,16 +1,12 @@
-import { Inter } from "next/font/google";
 import HeaderExample from "@/Components/HeaderExample";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`${inter.className}`}>
+    <main>
       <div className="flex flex-col gap-10 items-center py-24">
         <h1 className="font-black text-gray-800 text-6xl">
           react-resizable-grid-panels
         </h1>
-
         <HeaderExample />
       </div>
     </main>
