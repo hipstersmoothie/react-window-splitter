5905558e4598ce51d8dfb12ceef51069213177dd
Andrew Lisowski <lisowski54@gmail.com>
2024-08-08 21:21:54 -0700

    fix content match

--- packages/docs/tailwind.config.ts
+++ packages/docs/tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 import tailwindcssRadixColors from "tailwindcss-radix-colors";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
