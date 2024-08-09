07d295a7e0158c77a0ce35bbb28d3072828411e9
Andrew Lisowski <lisowski54@gmail.com>
2024-08-09 00:03:47 -0700

    fix linting

---
+++ packages/config-eslint/base..js
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import { fixupPluginRules } from "@eslint/compat";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  { ignores: ["**/dist/**", "**/.next/**"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "18",
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      "react/prop-types": "off",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    ...reactPlugin.configs.flat["jsx-runtime"],
+  },
+  {
+    plugins: {
+      "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+    },
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+    },
+  },
+];
